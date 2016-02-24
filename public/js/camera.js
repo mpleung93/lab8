52c94f2207b7fbc324a1d5e99b4fb46e6c096059
Marisa Leung <mpleung93@yahoo.com>
2016-02-24 19:10:24 +0000

    thirdpartyAPI lab

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,8 +1,7 @@
 function initCamera() {
-  //Uncomment and fill in the correct selectors below.
-  //capture($('<Your code here>'),
-  //        $('<Your code here>'),
-  //        $('<Your code here>'));
+  capture($('#camera-canvas'),
+          $('#camera-video'),
+          $('#camera-button'));
 }
 
 function capture(video, canvas, snapshotButton) {
